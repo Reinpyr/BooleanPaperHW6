27c1d1a06edb56675a1bedafc174b7be6c08fff5
Jaksa Jokunen <testinumero123@gmail.com>
2022-08-29 14:29:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int n = 143;
+        int j = 2;
+        boolean notprime = false;
+
+        while ((!notprime) && (j < n)) {
+            if (n % j == 0) {
+                System.out.println("The integer " + n + " is not a prime, because it's divisible atleast by " + j);
+                notprime = true;
+            } else {
+                j = j + 1;
+            }
+        }
+        if (!notprime) {
+            System.out.println("The integer " + n + " is a prime");
+        }
+    }
+}
