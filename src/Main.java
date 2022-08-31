70a61ba5987ec8f3fdbba6c2b474d0faea3b7454
Jaksa Jokunen <testinumero123@gmail.com>
2022-08-31 15:20:42 +0300

    Fixed the code and it works now while n==2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        int n = 143;
+        int n = 2;
         int j = 2;
         boolean notprime = false;
 
-        while ((!notprime) && (j < n)) {
+        while ((!notprime) && (j < n) || ((n == 2) && (notprime == false))) {
             if (n % j == 0) {
                 System.out.println("The integer " + n + " is not a prime, because it's divisible atleast by " + j);
                 notprime = true;
